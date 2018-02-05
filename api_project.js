05229821916c975f5c7b08043c7b0351d9d171ae
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-05 15:10:31 +0000

    Deploy EBM-2017-2018/linkapp to github.com/EBM-2017-2018/linkapp.git:gh-pages

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "ApiDoc pour le projet Linkapp",
+  "url": "/api",
+  "sampleUrl": "http://localhost:3000/api",
+  "name": "Linkapp",
+  "version": "1.0.0-SNAPSHOT",
+  "description": "",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-02-05T15:10:12.737Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
