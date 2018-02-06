b77818c43cc594486fd114c4af95eb09c99c6962
Yazid Smahi <yazidsmahi@gmail.com>
2018-02-06 14:29:32 +0100

    Connexion between front register button and backend

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -3,6 +3,8 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import axios from 'axios';
+import Login from './Login';
 
 
 class Register extends Component {
@@ -67,11 +69,52 @@ class Register extends Component {
     }
 
     handleClick(event){
-        console.log("event", event);
+        console.log("event", event); // TODO : delete these two lines
         console.log("values",this.state.first_name,this.state.last_name,this.state.email,this.state.username,this.state.password);
 
         // TODO:check for empty values before hitting submit
+        var apiBaseUrl = "https://linkapp.ebm.nymous.io/api/";
+        console.log("values",this.state.first_name,this.state.last_name,this.state.email,this.state.password);
+        //To be done:check for empty values before hitting submit
+        var self = this;
+        var donneesFormulaire={
+            "prenom": this.state.first_name,
+            "nom":this.state.last_name,
+            "email":this.state.email,
+            "username":this.state.username,
+            "password":this.state.password,
+            "role":"etudiant"
+        }
 
+        axios.post(apiBaseUrl+'/signup', donneesFormulaire, this.creerStructureFormulaire(donneesFormulaire))
+            .then(function (response) {
+                console.log(response);
+                if(response.data.code == 200){
+                    //  console.log("registration successfull");
+                    var loginscreen=[];
+                    loginscreen.push(<Login parentContext={this}/>);
+                    var loginmessage = "Not Registered yet.Go to registration";
+                    self.props.parentContext.setState({loginscreen:loginscreen,
+                        loginmessage:loginmessage,
+                        buttonLabel:"Register",
+                        isLogin:true
+                    });
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    creerStructureFormulaire(donneesFormulaire) {
+        var structureFormulaire = [];
+        for (var proprietes in donneesFormulaire) {
+            var encodedKey = encodeURIComponent(proprietes);
+            var encodedValue = encodeURIComponent(donneesFormulaire[proprietes]);
+            structureFormulaire.push(encodedKey + "=" + encodedValue);
+        }
+        structureFormulaire = structureFormulaire.join("&");
+        return structureFormulaire;
     }
 }
 
